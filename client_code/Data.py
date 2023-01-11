27a0262c772ba8efacfb515fb0cf61774c0787f0
kag@optim-al.com <kag@optim-al.com>
2023-01-11 13:02:53 +0000

    Created Data; Edited Form1

---
+++ client_code/Data.py
@@ -0,0 +1,14 @@
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from . import Module1
+#
+#    Module1.say_hello()
+#
+
+bp_sys = [142, 135, 134, 122, 138, 118]
+bp_dia = {90, 85, 85, 78, 82, 75}
+
+
+def say_hello():
+  print("Hello, world")
--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,8 +1,13 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import plotly.graph_objects as go
+# from . import Data
+
 
 class Form1(Form1Template):
+  bp_sys = [142, 135, 134, 122, 138, 118]
+  bp_dia = {90, 85, 85, 78, 82, 75}
+  
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
