5f5e624eb03d8df5a25c2a34a2913308822626ca
kag@optim-al.com <kag@optim-al.com>
2023-09-27 07:28:24 +0000

    Edited Analysis.RowTemplate3 and Analysis

--- client_code/Analysis/__init__.py
+++ client_code/Analysis/__init__.py
@@ -4,13 +4,12 @@ import plotly.graph_objects as go
 import anvil.server
 from .. import Data
 from ..afibs_g import afibs_g
-import time
+
 
 class Analysis(AnalysisTemplate):
   def __init__(self, **properties):
     print("Analysis  __init__")
     self.data_render()
-    time.sleep(1)
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Any code you write here will run before the form opens
@@ -18,4 +17,9 @@ class Analysis(AnalysisTemplate):
   def data_render(self):
     r = Data.set_comp_list("1001", 2, 'd', 360, "2023/08/01 00:00", "2023/08/11 00:00")
     self.repeating_panel_1.items = Data.comp_list
-    print(f"data_render()  ==> Analysis RP items= {self.repeating_panel_1.items}")
+    #print(f"data_render()  ==> Analysis RP items= {self.repeating_panel_1.items}")
+
+  def back_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Form1')
+
