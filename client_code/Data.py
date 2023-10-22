47d65c894331c908f0c2a35f5b9b11f1de5eac02
kag@optim-al.com <kag@optim-al.com>
2023-10-22 10:08:30 +0000

    Edited Data

--- client_code/Data.py
+++ client_code/Data.py
@@ -380,14 +380,14 @@ def set_comp_list(object: str, number: int, uom: str, Step: int, Tb1: str, Tb2: 
     return(-901)
 
   # Generate comp_list  #[{"no", "s1", "s2", "d1", "d2", "p1", "p2", "m1", "m2", "a1", "a2"}]
-  #print(f"set_comp_list() => Len1= {len(y_values)}  Len2= {len(y_values2)}")
   len1= len(y_values);  len2= len(y_values2)
   if len1 >= len2:
     min_len = len2-1
   else:
     min_len = len1-1
+  # print(f"set_comp_list() => Len1= {len(y_values)}  Len2= {len(y_values2)} min_len= {min_len}")
   for i in range(min_len):
-    if all or y_values[i][2]:    #        
+    if all or y_values[i][2]:    #
       comp_list.append({"no": y_values[i][1], "s1":y_values[i][2], "s2":y_values2[i][2], "d1":y_values[i][3],\
                         "d2":y_values2[i][3], "p1":y_values[i][4], "p2":y_values2[i][4],"m1":y_values[i][5],\
                         "m2":y_values2[i][5], "a1":y_values[i][6], "a2":y_values2[i][6]})        
