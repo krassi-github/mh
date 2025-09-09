c4f1b15a38a2d391051e7c52a81cdc74363c95c5
kag@optim-al.com <kag@optim-al.com>
2025-09-09 20:59:37 +0000

    Edited Afib_anal

--- client_code/Afib_anal/__init__.py
+++ client_code/Afib_anal/__init__.py
@@ -8,17 +8,51 @@ from .. import Data
 
 
 class Afib_anal(Afib_analTemplate):
+  id_title = ''
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     # Test on 09.09.2025  GPT rework 20-06-2025 ----------------------------------------
     # Задаваме `main_form` след създаване
     self.filter_1.set_main_form(self)
+    clm_date = [c for c in self.data_grid_1.columns if c['title'] == 'date'][0]
+    self.id_title = clm_date["id"]
     
     self.show_plot()
     self.show_grid()
 
 
+  def render_data(self, user, rng, Tb=None, Te=None, Step=None, crawl=False):   #  show_range  
+    # Form1.render_data(user, rng, Tb=None, Te=None, Step=None, crawl=Fals)
+    r = Data.set_bp_list(user, fr=rng, Tb=Tb, Te=Te, Step=Step, crawl=crawl)
+    if r < 0:
+      self.label_2.text += f"  set_bp_list= {r}"
+      self.label_2.foreground = "red"
+      self.label_2.boldface = True
+
+    if r < 0:
+      pass    # UI message to be generated
+    else:
+      self.repeating_panel_1.items = [row for row in Data.bp_list if row.get("afib") is not None and row.get("afib") != ""]
+      #self.repeating_panel_1.items = Data.bp_list
+      # print(f"RP.items = {self.repeating_panel_1.items}")
+      self.s_from.text = Data.loaded_from[:10]
+      self.s_to.text = "-     " + Data.loaded_to[:10]
+      self.s_tz.text = Data.zt_beg + " - " + Data.zt_end
+      if Data.slice_step:
+        self.s_step.text = Data.slice_step
+      else:
+        self.s_step.text = ''
+      
+      for i, c in enumerate(self.data_grid_1.columns):
+        if c["id"] == self.id_title:
+          if Data.slice_mode:
+            self.data_grid_1.columns[i]["title"] = 'SLICE'
+          else:
+            self.data_grid_1.columns[i]["title"] = 'DATE'
+      self.data_grid_1.columns = self.data_grid_1.columns 
+      
+      
   # Във форма Afib_anal, метод или бутон
   def show_plot(self, **event_args):
     fig = anvil.server.call('get_afib_figure')
@@ -28,4 +62,8 @@ class Afib_anal(Afib_analTemplate):
   def show_grid(self):
     # self.repeating_panel_1.items = list(filter(lambda row: row.get("afib"), Data.bp_list))
     self.repeating_panel_1.items = [row for row in Data.bp_list if row.get("afib") is not None and row.get("afib") != ""]
+
+  def back_click(self, **event_args):
+    open_form("Form1")
+    
 
