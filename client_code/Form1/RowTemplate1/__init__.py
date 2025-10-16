8c5ec5a259639c88ba0f912d72d0e2a039d0f34d
kag@optim-al.com <kag@optim-al.com>
2025-10-16 19:49:21 +0000

    Edited Form1.RowTemplate1

--- client_code/Form1/RowTemplate1/__init__.py
+++ client_code/Form1/RowTemplate1/__init__.py
@@ -10,7 +10,7 @@ class RowTemplate1(RowTemplate1Template):
     global r
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.link_1.tag = self.lb_1.text     # 09-06-2023 save the date
+    self.link_1.tag = self.lb_1.text     # save the date; self.lb_1.text
     self.lb_1.text = self.item["date"] if Data.current_range in ["d", "w"] \
     or Data.slice_mode is True else self.item["date"][:10]  # 20/09/2025, 09/10/2025
     self.row_spacing = 0
