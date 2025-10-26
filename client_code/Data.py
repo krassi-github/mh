afc621fa8ce371be8ab9eddc87c02b4ee4021987
kag@optim-al.com <kag@optim-al.com>
2025-10-26 17:04:41 +0000

    Edited Data

--- client_code/Data.py
+++ client_code/Data.py
@@ -323,7 +323,7 @@ def set_bp_list(user_id, fr=None, Tb=None, Te=None, Step=None, crawl=False, fill
 
       # prep data for the Slice modee -------------------
       _dt   = y_val[0][1]
-      if _dt[:4] != "2025":
+      if _dt and _dt[:4] != "2025":
         print(f"DateException {_dt} fr= {fr} Tb= {Tb} Te= {Te} sl= {zb + '-' + ze}  {y_val}")
       _afib = y_val[0][6]
       if _afib:
