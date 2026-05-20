084cb6b7cf39759ef94c9c1114d29f4ae9f9c14c
kag@optim-al.com <kag@optim-al.com>
2026-05-20 20:21:09 +0000

    Edited Data

--- client_code/Data.py
+++ client_code/Data.py
@@ -642,8 +642,8 @@ def afibs_year_summary(year: str):
     
     afibs_data = anvil.server.call(
       "get_afib_yearly_summary",
-      date_from="2021/01/01",	                #date_from,
-      date_to="2025/12/31",                   #date_to,
+      date_from="2021/01/01",	                #date_from,  // "2021/01/01"
+      date_to=time_to,                       #date_to,  //  "2025/12/31",
       zt_beg=zt_beg,
       zt_end=zt_end
     )
